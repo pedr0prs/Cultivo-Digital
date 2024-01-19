0a7f3d43d78e93848f469f97b53516a2d6913a2a
pedr0prs <pedr0prs.1997@gmail.com>
2024-01-19 17:43:53 -0300

    feat: first project with js

---
+++ ONE/logica-js-projeto_inicial/app.js
@@ -0,0 +1,16 @@
+alert('Boas-vindas ao jogo do número secreto');
+
+let numeroSecreto = 69;
+console.log(numeroSecreto)
+
+let chute = prompt('Escolha um número entre 50 e 70');
+
+if (chute == numeroSecreto ) {
+    alert(`Parabéns, você acertou! ${numeroSecreto}`);
+} else {
+    alert('ERRRRROOOUUU')
+}
+
+
+
+
