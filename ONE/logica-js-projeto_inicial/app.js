cb9613b5275ecb4baf42894ddaa7baa474f29b32
pedr0prs <pedr0prs.1997@gmail.com>
2024-01-19 23:28:35 -0300

    feat: number-game

--- ONE/logica-js-projeto_inicial/app.js
+++ ONE/logica-js-projeto_inicial/app.js
@@ -1,16 +1,24 @@
 alert('Boas-vindas ao jogo do número secreto');
 
-let numeroSecreto = 69;
-console.log(numeroSecreto)
+let numeroSecreto = 15;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1; 
 
-let chute = prompt('Escolha um número entre 50 e 70');
-
-if (chute == numeroSecreto ) {
-    alert(`Parabéns, você acertou! ${numeroSecreto}`);
-} else {
-    alert('ERRRRROOOUUU')
+// enquanto o chute não for igual ao numero secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto ) {
+        alert(`Parabéns, você acertou! ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas +1;
+        tentativas++;
+    }   
 }
-
-
-
 
